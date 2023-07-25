42e76f10ce69bc0e84b8e94939160b377fd3e7d0
mkoblako <mk@cloud.net.ua>
2023-07-25 06:09:23 +0300

    Update zimbra_services.sh

--- zimbra_services.sh
+++ zimbra_services.sh
@@ -12,6 +12,12 @@ zimbra_discovery="$(tail -n 1000 /var/log/zimbra-stats.log | grep STATUS | cut -
 case "$1" in
     discovery)
         echo -n '{"data":['
+        # Проверяем, существует ли файл лога
+        if [ ! -f "$zimbra_log_file" ]; then
+            echo "zimbra-stats.log doesn't exist"
+            exit 1
+        fi
+        
         for service in $zimbra_discovery; do
             echo -n "{\"{#ZIMBRASERVICE}\": \"$service\"},";
         done |sed -e 's:\},$:\}:'
@@ -24,6 +30,11 @@ case "$1" in
           exit 1
         fi
 
+        # Проверяем, содержит ли файл лога строки с ключевым словом "STATUS"
+        if ! /usr/bin/tail -n 1000 "$zimbra_log_file" | grep "STATUS" >/dev/null; then
+          echo "zimbra-stats.log doesn't contain STATUS entries"
+          exit 1
+        fi
         state="$(/usr/bin/tail -n 1000 $zimbra_log_file | grep STATUS | tail -1 | awk '{print $NF}')"
 
         if [ "$state" != "Running" ]; then
